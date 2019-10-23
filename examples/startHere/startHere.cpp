cd8f81f6a0ffa81d8ad107ac458b84f54d771f03
for-hk <53420891+for-hk@users.noreply.github.com>
2019-10-24 02:47:38 +0800

    read value from BLE

--- examples/startHere/startHere.cpp
+++ examples/startHere/startHere.cpp
@@ -66,7 +66,8 @@ bool onFlag = false;
 
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_NOTIFY "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_READ "6E400004-B5A3-F393-E0A9-E50E24DCCA9E"
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
@@ -99,6 +100,10 @@ class MyCallbacks: public BLECharacteristicCallbacks {
         Serial.println();
         Serial.println("*********");
       }
+    }
+
+    void onRead(BLECharacteristic *pCharacteristic) {
+      pCharacteristic->setValue("ABCD");
     }
 };
 
@@ -159,11 +164,18 @@ void setup() {
 
   // Create a BLE Characteristic
   pTxCharacteristic = pService->createCharacteristic(
-										CHARACTERISTIC_UUID_TX,
+										CHARACTERISTIC_UUID_NOTIFY,
 										BLECharacteristic::PROPERTY_NOTIFY
 									);
                       
   pTxCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic * pReadCharacteristic = pService->createCharacteristic(
+											 CHARACTERISTIC_UUID_READ,
+											BLECharacteristic::PROPERTY_READ
+										);
+
+  pReadCharacteristic->setCallbacks(new MyCallbacks());
 
   BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
 											 CHARACTERISTIC_UUID_RX,
