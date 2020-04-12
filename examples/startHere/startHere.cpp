cf9f0d2da7e571d0754401192b620304d00c0905
for-hk <53420891+for-hk@users.noreply.github.com>
2020-04-13 06:23:41 +0800

    enable Lora send and receive

--- examples/startHere/startHere.cpp
+++ examples/startHere/startHere.cpp
@@ -290,6 +290,8 @@ result idle(menuOut& o, idleEvent e) {
 #define BAND 866E6
 bool isLoRaReady = false;
 
+void loopLoRa();
+
 #endif
 
 // Handle OTA
@@ -391,6 +393,78 @@ void loopTTGOLEDDisplay() {
               read_message_queue.size());
     display_need_update = false;
   }
+}
+
+#endif
+
+#if LORA_MODE == ENABLE_LORA
+String outgoing;  // outgoing message
+
+byte msgCount = 0;         // count of outgoing messages
+byte localAddress = 0xBB;  // address of this device
+byte destination = 0xFF;   // destination to send to
+long lastSendTime = 0;     // last send time
+int interval = 2000;       // interval between sends
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();             // start packet
+  LoRa.write(destination);        // add destination address
+  LoRa.write(localAddress);       // add sender address
+  LoRa.write(msgCount);           // add message ID
+  LoRa.write(outgoing.length());  // add payload length
+  LoRa.print(outgoing);           // add payload
+  LoRa.endPacket();               // finish packet and send it
+  msgCount++;                     // increment message ID
+}
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;  // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();        // recipient address
+  byte sender = LoRa.read();          // sender address
+  byte incomingMsgId = LoRa.read();   // incoming msg ID
+  byte incomingLength = LoRa.read();  // incoming msg length
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {  // check length for error
+    Serial.println("error: message length does not match length");
+    return;  // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;  // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+}
+
+void loopLoRa() {
+  if (millis() - lastSendTime > interval) {
+    String message = "HeLoRa World 2!";  // send a message
+    sendMessage(message);
+    Serial.println("Sending " + message);
+    lastSendTime = millis();         // timestamp the message
+    interval = random(2000) + 1000;  // 2-3 seconds
+  }
+
+  // parse for a packet, and call onReceive with the result:
+  onReceive(LoRa.parsePacket());
 }
 
 #endif
@@ -724,12 +798,11 @@ void setup() {
 
   if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
-    while (1)
-      ;
+  } else {
+    Serial.println("LoRa Initializing OK!");
+    isLoRaReady = true;
   }
 
-  Serial.println("LoRa Initializing OK!");
-  isLoRaReady = true;
 #endif
 
   mesh.setDebugMsgTypes(ERROR | DEBUG | CONNECTION | SYNC);
@@ -897,6 +970,10 @@ void loop() {
   }
 
   ledcWrite(pwmLedChannelTFT, ledBacklight);
+#endif
+
+#if LORA_MODE == ENABLE_LORA
+  loopLoRa();
 #endif
 }
 
